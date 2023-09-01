72e4529a36b66d9d5efc1d6b9c5836b1dcd9c1da
Grace <gracesilvaduarte@gmail.com>
2023-08-31 21:02:53 -0500

    mostrar ventana: se agrega efecto de aparición de la ventana

---
+++ src/main.js
@@ -0,0 +1,9 @@
+function startCount(){
+    setTimeout(showWindow, 3000);
+}
+
+let windowBackground = document.getElementById("windowBackground");
+
+function showWindow(){
+    windowBackground.style.display="block";
+}
